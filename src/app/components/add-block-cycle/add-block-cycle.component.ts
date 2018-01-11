90a3a09e6edb2412f03c921126765f5669af97c1
shtaimoortariq <taimoortariqdev@gmail.com>
2018-01-11 22:22:07 +0500

    Step with api

--- src/app/components/add-block-cycle/add-block-cycle.component.ts
+++ src/app/components/add-block-cycle/add-block-cycle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FUNCTION_TYPE } from '@angular/compiler/src/output/output_ast';
 
 declare let $;
 
@@ -10,81 +11,58 @@ declare let $;
     styleUrls: ['./add-block-cycle.component.css']
 })
 export class AddBlockCycleComponent implements OnInit {
-    stepOne = true;
-    stepTwo = true;
-    stepThree = true;
-    stepFour = true;
+
     @Input('blockCycles') blockCycles: any;
 
-    abc: 'asdhaskdhkjashsdkahskdhaskjd'
-
+    tempArr = [];
+    blocksArray = [];
+    checkedArray = [];
+    date;
     constructor(private http: HttpClient) {
 
     }
+    
+    sendData() {
+
+        for (let i = 0; i < this.checkedArray.length; ++i) {
+            let tempValue = this.blocksArray.filter(
+                data => data.blockId === this.checkedArray[i].blockId);
+
+            console.log(tempValue);
+
+        }
+    };
+
+
 
     ngOnInit() {
         console.log(this.blockCycles)
+
+
+        
+        for (let i = 0; i < this.blockCycles.length; ++i) {
+
+            this.http.get('http://kybodev01.northeurope.cloudapp.azure.com/PestInspections/api/Blocks/Get/' + this.blockCycles[i].blockCycleId).subscribe(data => {
+                this.tempArr.push(data);
+                this.tempArr.map((data) => {
+                    data.map(newData => {
+                        this.blocksArray.push(newData);
+                        console.log(this.blocksArray)
+                    })
+                })
+            });
+
+        }
+
+        console.log(this.blocksArray);
+
+
         // Date Picker
         $('.mydatepicker, #datepicker').datepicker();
         $('#datepicker-autoclose').datepicker({
             autoclose: true,
             todayHighlight: true
         });
-        // jQuery('.mydatepicker, #datepicker').datepicker();
-
-
-        // $(".tab-wizard").steps({
-        //   headerTag: "h6"
-        //   , bodyTag: "section"
-        //   , transitionEffect: "fade"
-        //   , titleTemplate: '<span class="step">#index#</span> #title#'
-        //   , labels: {
-        //     finish: "Submit"
-        //   }
-        //   , onFinished: (event, currentIndex) => {
-        //     //  swal("Form Submitted!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat eleifend ex semper, lobortis purus sed.");
-        //   }
-        // });
-
-
-        // var form = $(".validation-wizard").show();
-
-        // $(".validation-wizard").steps({
-        //   headerTag: "h6"
-        //   , bodyTag: "section"
-        //   , transitionEffect: "fade"
-        //   , titleTemplate: '<span class="step">#index#</span> #title#'
-        //   , labels: {
-        //     finish: "Submit"
-        //   }
-        //   , onStepChanging: (event, currentIndex, newIndex) => {
-        //     return currentIndex > newIndex || !(3 === newIndex && Number($("#age-2").val()) < 18) && (currentIndex < newIndex && (form.find(".body:eq(" + newIndex + ") label.error").remove(), form.find(".body:eq(" + newIndex + ") .error").removeClass("error")), form.validate().settings.ignore = ":disabled,:hidden", form.valid())
-        //   }
-        //   , onFinishing: (event, currentIndex) => {
-        //     return form.validate().settings.ignore = ":disabled", form.valid()
-        //   }
-        //   , onFinished: (event, currentIndex) => {
-        //     //  swal("Form Submitted!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat eleifend ex semper, lobortis purus sed.");
-        //   }
-        // }), $(".validation-wizard").validate({
-        //   ignore: "input[type=hidden]"
-        //   , errorClass: "text-danger"
-        //   , successClass: "text-success"
-        //   , highlight: (element, errorClass) => {
-        //     $(element).removeClass(errorClass)
-        //   }
-        //   , unhighlight: (element, errorClass) => {
-        //     $(element).removeClass(errorClass)
-        //   }
-        //   , errorPlacement: (error, element) => {
-        //     error.insertAfter(element)
-        //   }
-        //   , rules: {
-        //     email: {
-        //       email: !0
-        //     }
-        //   }
-        // })
 
 
         $(".tab-wizard").steps({
@@ -95,52 +73,37 @@ export class AddBlockCycleComponent implements OnInit {
             , labels: {
                 finish: "Submit"
             }
-            , onFinished: function (event, currentIndex) {
-                this.swal("Form Submitted!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat eleifend ex semper, lobortis purus sed.");
-
+            , onFinished: () => {
+                this.sendData()
             }
         });
+    }
 
 
-        //   var form = $(".validation-wizard").show();
-
-        //   $(".validation-wizard").steps({
-        //       headerTag: "h6"
-        //       , bodyTag: "section"
-        //       , transitionEffect: "fade"
-        //       , titleTemplate: '<span class="step">#index#</span> #title#'
-        //       , labels: {
-        //           finish: "Submit"
-        //       }
-        //       , onStepChanging: function (event, currentIndex, newIndex) {
-        //           return currentIndex > newIndex || !(3 === newIndex && Number($("#age-2").val()) < 18) && (currentIndex < newIndex && (form.find(".body:eq(" + newIndex + ") label.error").remove(), form.find(".body:eq(" + newIndex + ") .error").removeClass("error")), form.validate().settings.ignore = ":disabled,:hidden", form.valid())
-        //       }
-        //       , onFinishing: function (event, currentIndex) {
-        //           return form.validate().settings.ignore = ":disabled", form.valid()
-        //       }
-        //       , onFinished: function (event, currentIndex) {
-        //            this.swal("Form Submitted!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat eleifend ex semper, lobortis purus sed.");
-        //       }
-        //   }), $(".validation-wizard").validate({
-        //       ignore: "input[type=hidden]"
-        //       , errorClass: "text-danger"
-        //       , successClass: "text-success"
-        //       , highlight: function (element, errorClass) {
-        //           $(element).removeClass(errorClass)
-        //       }
-        //       , unhighlight: function (element, errorClass) {
-        //           $(element).removeClass(errorClass)
-        //       }
-        //       , errorPlacement: function (error, element) {
-        //           error.insertAfter(element)
-        //       }
-        //       , rules: {
-        //           email: {
-        //               email: !0
-        //           }
-        //       }
-        //   })
+    createSelection(Index) {
+        let index = this.checkedArray.indexOf(this.blocksArray[Index]);
+        if (index < 0) {
+            let a = Object.assign({ assignDate: "" }, this.blocksArray[Index]);
+            console.log(a);
+            this.checkedArray.push(a);
+            console.log(this.checkedArray);
+        } else {
+            this.checkedArray.splice(index, 1);
+            console.log(this.checkedArray);
+        }
 
     }
 
+
+
+
+    abc(date, Indexx) {
+        console.log(date);
+        console.log(Indexx);
+    }
+
+
+    def() {
+        console.log(this.checkedArray);
+    }
 }
